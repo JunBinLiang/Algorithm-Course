800705b23ecd1142857ebbe2897ff558f85d04e4
66brother <45537132+JunBinLiang@users.noreply.github.com>
2023-04-02 22:07:59 -0400

    Create 121.cpp

---
+++ DP1-Introduction/121.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int res = 0;
+        int buy = prices[0];
+        //If sell at day i
+        for(int i = 1; i < prices.size(); i++) {
+            res = max(res, prices[i] - buy);
+            buy = min(buy, prices[i]);
+        } 
+        return res;
+    }
+};
