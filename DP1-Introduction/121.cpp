e7f542013fe3b1be4340d684f169e66b5eb47dee
66brother <45537132+JunBinLiang@users.noreply.github.com>
2023-04-02 22:13:41 -0400

    Update 121.cpp

--- DP1-Introduction/121.cpp
+++ DP1-Introduction/121.cpp
@@ -3,7 +3,9 @@ public:
     int maxProfit(vector<int>& prices) {
         int res = 0;
         int buy = prices[0];
-        //If sell at day i
+        //Sell at day i with prices[i]
+        //We need to buy at preices[0 : i - 1]
+        //The maximum we can earn is prices[i] = min(prices[0 : i - 1])
         for(int i = 1; i < prices.size(); i++) {
             res = max(res, prices[i] - buy);
             buy = min(buy, prices[i]);
